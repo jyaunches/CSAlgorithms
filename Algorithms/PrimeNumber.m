381ea112742e55380fe4bf27b02334cfcd193c71
Julie Yaunches <jmyaunch@gmail.com>
2015-10-18 20:35:35 -0400

    documentation for prime numbers under function

--- Algorithms/PrimeNumber.m
+++ Algorithms/PrimeNumber.m
@@ -26,16 +26,40 @@
 
     //refine candidates
     NSUInteger curEvalIndex = 1;  // should point to next candidate after 2 -> i.e. 3
-    NSNumber *curEval = candidates[curEvalIndex];
+    NSNumber *curPrime = candidates[curEvalIndex];
 
+    // We can stop when our currently evaluating prime number has reached the square root of the input
+    // because:
+    // sqrt(input) = n where -> n*n = input
+    // thus:
+    //   if n happens to be a prime number, we will have evaluated n and all multiples of n
+    //   if n is not a prime number, we will have evaluated all the prime numbers under it
+    //
+    // so:
+    //   by evaluating the multiples of all the primes under n we will have reached all numbers that are non-primes(multiples of other primes)
+    
+    // Input: 73
+    // Non prime subnumbers:
+    // 49 = 2^0 * 3^0 * 5^0 * 7^2 * 11^0 *  ...
+    // 57 = 2^0 * 3^1 * 5^0 * 7^2 * 11^0 * 13^0 * 17^0 * 19^1 *  ...
 
-    while ((curEvalIndex < candidates.count) && (curEval.doubleValue <= sqrt(input))) {
-        for (int i = curEval.intValue * curEval.intValue; i < input; i += curEval.intValue) {
+    // sqrt(73) = 8.5
+    // Last prime number to evaluate will be 7
+    // -> since 7^7 = 49... we can be confident that all non prime numbers between
+    // 49 and 73 will have been eliminated because they must contain a divisor of a
+    // prime number <= 7
+    // Because.... all non prime numbers are multiples of 2 other prime numbers...
+    // In this example the next prime number after 7 is 13... now there may be numbers
+    // between 49 and 73 that are multiples of 13... but that multiplier would have to be < 7
+    // meaning as long as we evaluated the lower primes, we will have already removed it
+    
+    while (curPrime.doubleValue <= sqrt(input)) {
+        for (int i = curPrime.intValue * curPrime.intValue; i < input; i += curPrime.intValue) {
             [candidates removeObject:@(i)];
         }
 
         curEvalIndex++;
-        curEval = candidates[curEvalIndex];
+        curPrime = candidates[curEvalIndex];
     }
     return candidates;
 }
